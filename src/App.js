94378f200e8b2ce5cdddfa1feaf77819b40b740d
ragha4ru <70031232+ragha4ru@users.noreply.github.com>
2021-12-15 07:02:46 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ExampleFunc1 from './ExampleFunc1';
 function App() {
   return (
     <div className="App">
-      <h1>Demo 
+      <h1>Demo 1
       </h1>
       <hr></hr>
      <ExampleFunc1></ExampleFunc1>
--- src/ExampleFunc1.js
+++ src/ExampleFunc1.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ExampleFunc1 = () => {
   return (
     <div>
-      <h1>Array frunction components </h1>
+      <h1>Array frunction components 1</h1>
     </div>
   );
 };
